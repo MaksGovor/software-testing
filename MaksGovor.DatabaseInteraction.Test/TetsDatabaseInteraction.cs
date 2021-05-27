3d495f09f51978f951c1d4c6ec64b9a2b28a7b79
MaksGovor <maksgovruha@gmail.com>
2021-05-27 19:28:01 +0300

    [Lab 4] Update tests

--- MaksGovor.DatabaseInteraction.Test/TetsDatabaseInteraction.cs
+++ MaksGovor.DatabaseInteraction.Test/TetsDatabaseInteraction.cs
@@ -8,7 +8,7 @@ using System;
 namespace MaksGovor.DatabaseInteraction.Test
 {
     [TestClass]
-    public class TetsDatabaseInteraction
+    public class TetsDatabaseInteractionAuth
     {
         private const string Server = @"DESKTOP-NILDRFH";
         private const string Database = @"IIG.CoSWE.AuthDB";
@@ -16,21 +16,31 @@ namespace MaksGovor.DatabaseInteraction.Test
         private const string Login = @"sa";
         private const string Password = @"26052002";
         private const int ConnectionTimeout = 75;
+        private AuthDatabaseUtils authDatabase;
 
-        [TestMethod]
-        public void TestMethod1()
+        [TestInitialize]
+        public void Initialization()
+        {
+            authDatabase = new AuthDatabaseUtils(
+                Server, Database, IsTrusted, Login, Password, ConnectionTimeout
+            );
+        }
+
+        [DataTestMethod]
+        [DataRow("user1", "genabukintop", "salt64last", (uint)1)]
+        [DataRow("user2", "qwerty1", "", uint.MaxValue)]
+        [DataRow("user3", "абвгд", "网络")]
+        [DataRow("user4", ",*₴!::", "salt64last")]
+        public void TestMethod1(string login, string password, string salt = null, uint? adlerMod = null)
         {
             try
             {
-                AuthDatabaseUtils authDatabase = new AuthDatabaseUtils(
-                    Server, Database, IsTrusted, Login, Password, ConnectionTimeout
-                    );
-                authDatabase.AddCredentials("eee2y", "ieeerrrrieeerrrrieeerrrrieeerrrrieeerrrrieeerrrrieeerrrrieeerrrrieeerrrr");
+                string hash = PasswordHasher.GetHash(password, salt, adlerMod);
+                Assert.IsTrue(authDatabase.AddCredentials(login, hash));
             } catch (Exception err)
             {
                 Assert.Fail(err.Message);
             }
-
         }
     }
 }
